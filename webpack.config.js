e53f3f6b85c73eecd6ea1852697c1c4736d0cab1
SCHEMR <schemryj@gmail.com>
2016-05-22 20:45:02 +0900

    Analyzing Build Statistics

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,8 @@ const PATHS = {
     style: path.join(__dirname, 'app', 'main.css'),
     build: path.join(__dirname, 'build')
 };
+const TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
 
 const common = {
     // Entry accepts a path or an object of entries.
@@ -31,43 +33,41 @@ const common = {
     ]
 };
 var config;
-// Detect how npm is run and branch based on that
-switch(process.env.npm_lifecycle_event) {
-    case 'build':
-        config = merge(
-            common,
-            {
-                devtool: 'source-map',
-                output: {
-                    path: PATHS.build,
-                    filename: '[name].[chunkhash].js',
-                    // This is used for require.ensure. The setup
-                    // will work without but this is useful to set.
-                    chunkFilename: '[chunkhash].js'
-                }
-            },
-            parts.clean(PATHS.build),
-            parts.extractBundle({
-                name: 'vendor',
-                entries: ['react']
-            }),
-            parts.minify(),
-            parts.extractCSS(PATHS.style)
-        );
-        break;
-    default:
-        config = merge(
-            common,
-            {
-                devtool: 'eval-source-map'
-            },
-            parts.setupCSS(PATHS.style),
-            parts.devServer({
-                // Customize host/port here if needed
-                host: process.env.HOST,
-                port: process.env.PORT
-            })
-        )
-};
+if(TARGET === 'build' || TARGET === 'stats') {
+    config = merge(
+        common,
+        {
+            devtool: 'source-map',
+            output: {
+                path: PATHS.build,
+                filename: '[name].[chunkhash].js',
+                // This is used for require.ensure. The setup
+                // will work without but this is useful to set.
+                chunkFilename: '[chunkhash].js'
+            }
+        },
+        parts.clean(PATHS.build),
+        parts.extractBundle({
+            name: 'vendor',
+            entries: ['react']
+        }),
+        parts.minify(),
+        parts.extractCSS(PATHS.style)
+    );
+}
+if(TARGET === 'start' || !TARGET) {
+    config = merge(
+        common,
+        {
+            devtool: 'eval-source-map'
+        },
+        parts.setupCSS(PATHS.style),
+        parts.devServer({
+            // Customize host/port here if needed
+            host: process.env.HOST,
+            port: process.env.PORT
+        })
+    )
+}
 
 module.exports = validate(config)
